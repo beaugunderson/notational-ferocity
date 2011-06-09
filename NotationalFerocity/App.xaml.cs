8a305c8502332e73b616d986777eef2b21eb844b
Beau Gunderson <beau@beaugunderson.com>
2011-06-08 21:03:48 -0700

    Beginning to add logging support with NLog.

--- NotationalFerocity/App.xaml.cs
+++ NotationalFerocity/App.xaml.cs
@@ -1,9 +1,66 @@
-﻿namespace NotationalFerocity
+﻿using System.Windows;
+
+using NLog;
+
+namespace NotationalFerocity
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App
     {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Logger.Info("Application starting.");
+
+            Current.DispatcherUnhandledException += OnDispatcherUnhandledException;
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            Logger.Info("Application exiting.");
+
+            base.OnExit(e);
+        }
+
+        private bool showingFatal;
+
+        void OnDispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            e.Handled = true;
+
+            if (showingFatal)
+            {
+                e.Handled = true;
+
+                return;
+            }
+
+            showingFatal = true;
+
+            Logger.Error("Fatal exception ".PadLeft(80, '*'));
+
+            try
+            {
+                int level = 0;
+
+                var ex = e.Exception;
+
+                while (ex != null)
+                {
+                    Logger.Error(string.Format("Fatal exception level {0}: ", level++), ex);
+                    
+                    ex = ex.InnerException;
+                }
+            }
+            finally
+            {
+               Logger.Error("Fatal exception end ".PadLeft(80, '*'));
+            }
+
+            Shutdown();
+        }
     }
 }
